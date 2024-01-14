3103d14c42d8aed3bd60433d2193e49870e1a9c9
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2024-01-14 16:16:27 +0000

    added questions to the questions variable as an array of objects

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter project description"
+    }, 
+    {
+        type: "input",
+        name: "installation",
+        message: "Project installation (how/where to download your program)"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Project usage (input examples of how your project can be used)"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "project license (choose from the list the license that this project is distributed under)",
+        choices: ["Apache license 2.0", "Boost Software License 1.0", "Creative Commons license family", "Eclipse Public License 1.0", "ISC", "MIT"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Project contribution (state if you are open to contributions and acceptance criteria)"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Project tests (include tests for your application and provide code examples and how to run them)"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your gitHub username',
+    }
 
 ];
 
